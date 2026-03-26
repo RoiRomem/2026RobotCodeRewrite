554f2c663082e1c8a24533da6f6136ae1ec73a9c
Roi Romem <130078520+RoiRomem@users.noreply.github.com>
2026-03-26 14:45:11 +0200

    Added IntakeButton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,13 +2,20 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
+
+  private CommandXboxController driverController = new CommandXboxController(0);
+  private Trigger IntakeButton = driverController.leftTrigger();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
