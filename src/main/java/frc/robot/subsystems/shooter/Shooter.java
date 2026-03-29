f0bce193eb1d252cfbceeec3ea6b6dc267536b90
Roi Romem <130078520+RoiRomem@users.noreply.github.com>
2026-03-29 21:45:54 +0300

    chore added basic structure

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.shooter;
+
+import team6230.koiupstream.io.UpstreamIO;
+import team6230.koiupstream.subsystems.UpstreamSubsystem;
+
+public class Shooter extends UpstreamSubsystem {
+
+    public Shooter() {
+        super("Shooter", null);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    protected UpstreamIO getIO() {
+
+    }
+
+}
